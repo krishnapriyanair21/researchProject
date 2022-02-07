ae5bcadacf9edc8fe058877915b7983366320b45
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2022-02-06 17:53:55 -0800

    adding testing

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
-const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
+
 
 // configure Handlebars view engine
 app.engine('handlebars', engine({
@@ -15,23 +16,15 @@ app.use(express.static(__dirname + '/public'))
 const port = process.env.PORT || 3000
 
 // GET
-app.get('/', (req, res) => res.render('home'))
-app.get('/about', (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-  res.render('about', { fortune: fortune.getFortune() })
-})
+app.get('/', handlers.home)
+
+app.get('/about', handlers.about)
+
 // custom 404 page
-app.use((req, res) => {
-  res.status(404)
-  res.render('404')
-})
+app.use(handlers.notFound)
 
 // custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
   `Express started on http://localhost:${port}; ` +
